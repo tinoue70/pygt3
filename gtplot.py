230a6c48f82a747493bee234b2a3e46bc711126a
Takahiro INOUE <tinoue@rist.jp>
2018-06-15 13:29:34 +0900

    Modified to use GT3Axis in gtplot.

--- gtplot.py
+++ gtplot.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 from __future__ import print_function
 # import numpy as np
-from pygt3file import GT3File
+from pygt3file import GT3File, GT3Axis
 import argparse
 import sys
-import os
 import matplotlib.pyplot as plt
 # import mpl_toolkits.basemap as basemap
 # from cartopy import config
@@ -15,30 +14,9 @@ class A:
     """ base class for argparse,"""
     pass
 
-
-def find_axfile(name, search_path=[u".", u"$GT3AXISDIR", u"$GTOOLDIR/gt3"]):
-    """
-    Find gtool3 axis file with given axis name `name` from path listed as `search_path`.
-
-    Return path of the found axis file or `None` unless found.
-    """
-    axis_path = map(os.path.expandvars, search_path)
-    # axis_path = [a for a in axis_path if a.find('$') < 0]
-    axis_path = [a for a in axis_path if os.path.exists(a)]
-
-    if (opt_debug):
-        print('*** axis_path:')
-        print(axis_path)
-
-    axfile = None
-    for axdir in axis_path:
-        # print(axdir)
-        axfile = os.path.join(axdir, 'GTAXLOC.'+name)
-        if (os.path.exists(axfile)):
-            break
-
-    # print("dbg:find_axfile:",axfile)
-    return axfile
+################################################################################
+# Here We Go
+################################################################################
 
 
 parser = argparse.ArgumentParser(description='Plot one data in gt3file')
@@ -102,7 +80,7 @@ if (opt_show_table):
 
 
 if (opt_data_number not in range(f.num_of_data)):
-    print('Error: data number out of range: %d is not in range(%d)' 
+    print('Error: data number out of range: %d is not in range(0:%d)'
           % (opt_data_number, f.num_of_data))
     sys.exit(1)
 
@@ -117,8 +95,8 @@ while True:
     f.read_one_header()
     if (f.is_eof):
         break
-    if (opt_debug):
-        print('dbg: current data:', f.current_header.number)
+    # if (opt_debug):
+    #     print('dbg: current data:', f.current_header.number)
     if (opt_data_number == f.current_header.number):
         if (opt_debug):
             print("dbg: data #%d found." % opt_data_number)
@@ -143,69 +121,42 @@ if (opt_debug):
 
 
 if (opt_vert_level not in range(target_data.shape[0])):
-    print('Error: vertical level out of range: %d is not in range(%d)' 
+    print('Error: vertical level out of range: %d is not in range(%d)'
           % (opt_vert_level, target_data.shape[0]))
     sys.exit(1)
 
 ################################################################################
 # Prepare axis data
 ################################################################################
-xax = {}
-yax = {}
 
-xax["name"] = target_header.aitm1
-xax["indices"] = (target_header.astr1, target_header.aend1)
-yax["name"] = target_header.aitm2
-yax["indices"] = (target_header.astr2, target_header.aend2)
-
-
+xax = GT3Axis(target_header.aitm1)
+if (xax.file is None):
+    sys.exit(1)
 if (opt_debug):
-    print("dbg xax:", xax)
-xf = GT3File(find_axfile(xax["name"]))
-xf.scan()
-xf.read_one_header()
-xf.read_one_data()
-if (opt_debug):
-    xf.dump_current_header()
-    xf.dump_current_data()
-xaxdata = xf.current_data.flatten()
-if (xf.current_header.cyclic):
-    xaxdata = xaxdata[:-1]
-# if (xf.current_header.cyclic):
-#     target_data,xaxdata = basemap.addcyclic(target_data,xaxdata[:-1])
-xf.close()
+    xax.dump()
 
+yax = GT3Axis(target_header.aitm2)
+if (yax.file is None):
+    sys.exit(1)
 if (opt_debug):
-    print("dbg yax:", yax)
-yf = GT3File(find_axfile(yax["name"]))
-yf.scan()
-yf.read_one_header()
-yf.read_one_data()
-if (opt_verbose):
-    yf.dump_current_header()
-    yf.dump_current_data()
-yaxdata = yf.current_data.flatten()
-if (yf.current_header.cyclic):
-    yaxdata = xaxdata[:-1]
-yf.close()
-
+    yax.dump()
 
 ################################################################################
 # Start plotting
 ################################################################################
 if (opt_debug):
     print("dbg: target_data.shape:", target_data.shape)
-    print("dbg: xaxdata.shape:", xaxdata.shape)
-    print("dbg: yaxdata.shape:", yaxdata.shape)
+    print("dbg: xax.data.shape:", xax.data.shape)
+    print("dbg: yax.data.shape:", yax.data.shape)
 
 # ax = plt.axes(projection=ccrs.Mollweide())
 # ax = plt.axes(projection=ccrs.NorthPolarStereo())
 ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=180.0))
-img = ax.contourf(xaxdata, yaxdata, target_data[opt_vert_level, :, :], 60,
+img = ax.contourf(xax.data, yax.data, target_data[opt_vert_level, :, :], 60,
                   transform=ccrs.PlateCarree())
 
 ax.axis("image")
-title = "%s[%s]" % (target_header.titl, target_header.unit)
+title = "%s:%s[%s]" % (target_header.item, target_header.titl, target_header.unit)
 ax.set(title=title)
 ax.coastlines()
 
