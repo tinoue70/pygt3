bfb489b679f65b77165f5026ea066c50c4d86698
Takahiro INOUE <tinoue@rist.jp>
2018-06-21 16:53:42 +0900

    Changed parsing arguments.

--- gtshow.py
+++ gtshow.py
@@ -11,108 +11,94 @@ class A:
     pass
 
 
+def set_idx_range(idx):
+    if (idx is None):
+        idx = ()
+    if (len(idx) == 0):
+        pass
+    elif (len(idx) == 1):
+        idx.append(idx[0]+1)
+    else:
+        idx = idx[:2]
+        idx.sort()
+    return idx
+
 description='Show contents of gt3file.'
 
 epilog="""
 Index range must be single integer to be sliced at, or two integers to
-be a range.  Note that these are treated as a Python manner, that is,
-starts from zero and second index is excluded.
+be a range.  Note that these are treated as a slice object, that is,
+`-x 1` shows data[:,:,1], but `-x 0 1` shows data[:,:,0:1] so data[:,:,1]
+is excluded. But NEVER use negative index.
 """
 
+###############################################################################
+# Here we go
+###############################################################################
 parser = argparse.ArgumentParser(
     description=description,
     epilog=epilog)
-
-
-parser.add_argument(
-    '-H', '--header', help='Show header only.',
-    action='store_true')
-parser.add_argument(
-    '-T', '--table', help='Show data table only.',
-    action='store_true')
-parser.add_argument(
-    'file', help='file name.')
-
-parser.add_argument(
-    '-i', '--indexed', help='Show data with grid indices',
-    action='store_true', default=False)
-
-parser.add_argument(
-    '-n', '--numbers', help='Space separated data number(s) to be shown.',
-    type=int, nargs='*')
 
 parser.add_argument(
     '-d', '--debug', help='debug output',
     action='store_true')
 
 parser.add_argument(
+    'file', help='gt3 file name.')
+
+# How to show
+parser.add_argument(
+    '-H', '--header_only', help='Show header only.',
+    action='store_true')
+parser.add_argument(
+    '-T', '--show_table', help='Show data table only.',
+    action='store_true')
+parser.add_argument(
+    '-i', '--indexed', help='Show data with grid indices',
+    action='store_true', default=False)
+
+# What to show
+parser.add_argument(
+    '-n', '--numbers', help='Space separated data number(s) to be shown.',
+    type=int, nargs='+', default=())
+parser.add_argument(
     '-x', '--xidx', help='index range X-direction.',
-    type=int, nargs='+', default=None)
+    type=int, nargs='+', default=())
 parser.add_argument(
-    '-y', '--yidx', nargs='+', help='index range Y-direction.',
-    type=int, default=None)
+    '-y', '--yidx', help='index range Y-direction.',
+    type=int, nargs='+', default=())
 parser.add_argument(
-    '-z', '--zidx', nargs='+', help='index range Z-direction.',
-    type=int, default=None)
+    '-z', '--zidx', help='index range Z-direction.',
+    type=int, nargs='+', default=())
 
-    
-a = A()
-parser.parse_args(namespace=a)
 
-file = a.file
+opt = A()
+parser.parse_args(namespace=opt)
 
-if (a.numbers is None):
-    opt_numbers = ()
-    opt_all = True
-else:
-    opt_numbers = tuple(a.numbers)
-    opt_all = False
+opt.all = (len(opt.numbers) == 0)
 
-opt_header_only = a.header
-opt_show_table = a.table
-opt_indexed = a.indexed
-opt_debug = a.debug
+opt.xidx = set_idx_range(opt.xidx)
+opt.yidx = set_idx_range(opt.yidx)
+opt.zidx = set_idx_range(opt.zidx)
 
-if (a.xidx is None):
-    opt_xidx = None
-else:
-    if (len(a.xidx) == 1):
-        opt_xidx = a.xidx[0]
-    else:
-        opt_xidx = tuple(a.xidx[:2])  # should be sorted after sliced?
 
-if (a.yidx is None):
-    opt_yidx = None
-else:
-    if (len(a.yidx) == 1):
-        opt_yidx = a.yidx[0]
-    else:
-        opt_yidx = tuple(a.yidx[:2])  # should be sorted after sliced?
-if (a.zidx is None):
-    opt_zidx = None
-else:
-    if (len(a.zidx) == 1):
-        opt_zidx = a.zidx[0]
-    else:
-        opt_zidx = tuple(a.zidx[:2])  # should be sorted after sliced?
-
-if (opt_debug):
+if (opt.debug):
     print("====== Options ======")
-    print("  opt_header_only:", opt_header_only)
-    print("  opt_show_table:", opt_show_table)
-    print("  opt_indexed", opt_indexed)
-    print("  opt_numbers:", opt_numbers)
-    print("  opt_xidx:", opt_xidx)
-    print("  opt_yidx:", opt_yidx)
-    print("  opt_zidx:", opt_zidx)
-    print("  file:", file)
+    print("  opt.header_only:", opt.header_only)
+    print("  opt.show_table:", opt.show_table)
+    print("  opt.indexed", opt.indexed)
+    print("  opt.numbers:", opt.numbers)
+    print("  opt.xidx:", opt.xidx)
+    print("  opt.yidx:", opt.yidx)
+    print("  opt.zidx:", opt.zidx)
+    print("  file:", opt.file)
 
 
-f = GT3File(file)
-f.opt_debug = opt_debug
+f = GT3File(opt.file)
+f.opt_debug = opt.debug
 f.scan()
 
-if (opt_show_table):
+if (opt.show_table):
     f.show_table()
     sys.exit(0)
 
@@ -120,16 +106,16 @@ while True:
     f.read_one_header()
     if (f.is_eof):
         break
-    if (opt_all or f.current_header.number in opt_numbers):
+    if (opt.all or f.current_header.number in opt.numbers):
         f.dump_current_header()
 
-    if (opt_header_only):
+    if (opt.header_only):
         f.skip_one_data()
     else:
-        if (opt_all or f.current_header.number in opt_numbers):
+        if (opt.all or f.current_header.number in opt.numbers):
             f.read_one_data()
-            f.dump_current_data(xidx=opt_xidx, yidx=opt_yidx, zidx=opt_zidx,
-                                indexed=opt_indexed)
+            f.dump_current_data(xidx=opt.xidx, yidx=opt.yidx, zidx=opt.zidx,
+                                indexed=opt.indexed)
         else:
             f.skip_one_data()
 
