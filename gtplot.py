d0a909a756c4a9085becd1e8b8f8b37beba522f7
Takahiro INOUE <tinoue@rist.jp>
2018-06-21 17:10:35 +0900

    Changed parsing arguments in gtplot.

--- gtplot.py
+++ gtplot.py
@@ -51,49 +51,49 @@ parser.add_argument(
     '--axdir', help='axis file directory.',
     default=None)
 
-a = A()
-parser.parse_args(namespace=a)
+opt = A()
+parser.parse_args(namespace=opt)
 
-file = a.file
+file = opt.file
 
-opt_data_number = a.number
-opt_vert_level = a.level
-if (a.axdir is not None):
-    opt_axdir = a.axdir.split(':')
+opt.data_number = opt.number
+opt.vert_level = opt.level
+if (opt.axdir is not None):
+    opt.axdir = opt.axdir.split(':')
 else:
-    opt_axdir = None
+    opt.axdir = None
 
-opt_show_table = a.table
-opt_debug = a.debug
-opt_verbose = a.verbose
+opt.show_table = opt.table
+opt.debug = opt.debug
+opt.verbose = opt.verbose
 
-opt_contour = a.contour
+opt.contour = opt.contour
 
-if (opt_debug):
-    opt_verbose = True
+if (opt.debug):
+    opt.verbose = True
 
-if (opt_debug):
-    print("dbg:opt_contour:", opt_contour)
-    print("dbg:opt_show_table:", opt_show_table)
-    print("dbg:opt_data_number:", opt_data_number)
-    print("dbg:opt_vert_level:", opt_vert_level)
-    print("dbg:opt_axdir:", opt_axdir)
+if (opt.debug):
+    print("dbg:opt.contour:", opt.contour)
+    print("dbg:opt.show_table:", opt.show_table)
+    print("dbg:opt.data_number:", opt.data_number)
+    print("dbg:opt.vert_level:", opt.vert_level)
+    print("dbg:opt.axdir:", opt.axdir)
     print("dbg:file:", file)
 
 f = GT3File(file)
-f.opt_debug = opt_debug
-f.opt_verbose = opt_verbose
+f.opt_debug = opt.debug
+f.opt_verbose = opt.verbose
 
 f.scan()
 
-if (opt_show_table):
+if (opt.show_table):
     f.show_table()
     sys.exit(0)
 
 
-if (opt_data_number not in range(f.num_of_data)):
+if (opt.data_number not in range(f.num_of_data)):
     print('Error: data number out of range: %d is not in range(0:%d)'
-          % (opt_data_number, f.num_of_data))
+          % (opt.data_number, f.num_of_data))
     sys.exit(1)
 
 ###############################################################################
@@ -107,9 +107,9 @@ while True:
     f.read_one_header()
     if (f.is_eof):
         break
-    if (opt_data_number == f.current_header.number):
-        if (opt_debug):
-            print("dbg: data #%d found." % opt_data_number)
+    if (opt.data_number == f.current_header.number):
+        if (opt.debug):
+            print("dbg: data #%d found." % opt.data_number)
         f.read_one_data()
         break
     else:
@@ -117,10 +117,10 @@ while True:
 
 target_header = f.current_header
 target_data = f.current_data
-if (opt_verbose):
+if (opt.verbose):
     target_header.dump()
 
-if (opt_debug):
+if (opt.debug):
     print("dbg: target_data:")
     print("* frags:")
     print(target_data.flags)
@@ -130,40 +130,40 @@ if (opt_debug):
     print("* ndim:", target_data.ndim)
     print("* shape:", target_data.shape)
 
-if (opt_vert_level not in range(target_data.shape[0])):
+if (opt.vert_level not in range(target_data.shape[0])):
     print('Error: vertical level out of range: %d is not in range(:%d)'
-          % (opt_vert_level, target_data.shape[0]))
+          % (opt.vert_level, target_data.shape[0]))
     sys.exit(1)
 
 ###############################################################################
 # Prepare axis data
 ###############################################################################
 
-xax = GT3Axis(target_header.aitm1, opt_axdir)
+xax = GT3Axis(target_header.aitm1, opt.axdir)
 if (xax.file is None):
     sys.exit(1)
-if (opt_debug):
+if (opt.debug):
     xax.dump()
 
-yax = GT3Axis(target_header.aitm2, opt_axdir)
+yax = GT3Axis(target_header.aitm2, opt.axdir)
 if (yax.file is None):
     sys.exit(1)
-if (opt_debug):
+if (opt.debug):
     yax.dump()
 
-zax = GT3Axis(target_header.aitm3, opt_axdir)
+zax = GT3Axis(target_header.aitm3, opt.axdir)
 if (zax.file is None):
     sys.exit(1)
-if (opt_debug):
+if (opt.debug):
     zax.dump()
 
-zlevel = zax.data[opt_vert_level]
+zlevel = zax.data[opt.vert_level]
 
 ###############################################################################
 # Start plotting
 ###############################################################################
 
-# if (opt_debug):
+# if (opt.debug):
 #     print("dbg: target_data.shape:", target_data.shape)
 #     print("dbg: xax.data.shape:", xax.data.shape)
 #     print("dbg: yax.data.shape:", yax.data.shape)
@@ -171,23 +171,23 @@ zlevel = zax.data[opt_vert_level]
 # 1D plot
 # fig = plt.figure
 # ax = plt.axes()
-# plt.plot(yax.data,target_data[opt_vert_level, :, 1])
+# plt.plot(yax.data,target_data[opt.vert_level, :, 1])
 # plt.show()
 
 # 2D plot
 # ax = plt.axes(projection=ccrs.Mollweide())
 # ax = plt.axes(projection=ccrs.NorthPolarStereo())
 ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=180.0))
-if (opt_contour):
-    img = ax.contour(xax.data, yax.data, target_data[opt_vert_level, :, :],
+if (opt.contour):
+    img = ax.contour(xax.data, yax.data, target_data[opt.vert_level, :, :],
                      10, transform=ccrs.PlateCarree())
 else:
-    img = ax.contourf(xax.data, yax.data, target_data[opt_vert_level, :, :],
+    img = ax.contourf(xax.data, yax.data, target_data[opt.vert_level, :, :],
                       60, transform=ccrs.PlateCarree())
 ax.axis("image")
 title = "%s:%s[%s] lvl=%g[%s]" % (
     target_header.item, target_header.titl, target_header.unit,
-    zax.data[opt_vert_level], zax.header.unit)
+    zax.data[opt.vert_level], zax.header.unit)
 ax.set(title=title)
 ax.coastlines()
 plt.colorbar(img, ax=ax, orientation="horizontal", extend="both")
