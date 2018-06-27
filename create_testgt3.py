97ba727fb35894988523d7fce0fe128bbb699650
Takahiro INOUE <tinoue@rist.jp>
2018-06-27 13:47:08 +0900

    Fixed to follow update on GT3Header.

--- create_testgt3.py
+++ create_testgt3.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 import numpy as np
+# import pandas as pd
 from pygt3file import GT3File, GT3Header
-from datetime import datetime, timedelta
+# import sys
 
 f = GT3File('testgt3', mode='wb')
 
-epoch = datetime(1,1,1,0,0,0)
-init_date = datetime(2018,6,19,0,0,0)
-init_time = (init_date-epoch).days*24
+init_date = "20180619 000000"
 
 date = init_date
-time = init_time
 deltaT = 3  # hour
 
 d = np.zeros(shape=(1,32,64), dtype='>f4')
@@ -18,7 +16,7 @@ d = np.zeros(shape=(1,32,64), dtype='>f4')
 f.current_header = GT3Header(
     dset='test',
     item='hoge', unit='-', title='test data',
-    date=date, utim='HOUR', time=time,
+    date=date, utim='HOUR',
     aitm1='GLON64', astr1=1, aend1=64,
     aitm2='GGLA32', astr2=1, aend2=32,
     aitm3='SFC1', astr3=1, aend3=1,
@@ -30,10 +28,8 @@ f.write_one_header()
 f.write_one_data()
 
 for i in range(11):
-    time = time + deltaT
-    date = date + timedelta(hours=deltaT)
-    f.current_header.date = date
-    f.current_header.time = time
+    time = f.current_header.time + deltaT
+    f.current_header.set_time_date(time=time)
 
     f.write_one_header()
     f.write_one_data()
