7fbf26507efc1acc7189039c7df57bfd48f3f3b3
Takahiro INOUE <tinoue@rist.jp>
2018-06-29 16:54:16 +0900

    Added several commandline option to create_testgt3.py.

--- create_testgt3.py
+++ create_testgt3.py
@@ -3,23 +3,77 @@ import numpy as np
 # import pandas as pd
 from pygt3file import GT3File, GT3Header
 # import sys
+import argparse
 
+class A:
+    """ general purpose bare class."""
+    pass
+
+###############################################################################
+# Here We Go
+###############################################################################
+
+item = 'u-velocity'
+title = 'eastward velocity'
+unit = 'm/s'
 
 init_date = "20180619 000000"
-
 date = init_date
-deltaT = 3  # hour
+deltaT = 3*3600  # 3hour
+utim = 'sec'
 
-d = np.zeros(shape=(1,32,64), dtype='>f4')
 
-with GT3File('testgt3', mode='wb') as f:
+parser = argparse.ArgumentParser(description='Plot one data in gt3file')
+
+parser.add_argument(
+    '-d', '--debug', help='debug output',
+    action='store_true')
+parser.add_argument(
+    '-v', '--verbose', help='verbose output',
+    action='store_true')
+
+parser.add_argument(
+    '-o', '--ofile', help='output netcdf file name.',
+    default='new.gt3')
+parser.add_argument(
+    '-n', '--num_times', help='number of time steps.',
+    type=int, default=5)
+parser.add_argument(
+    '--item', help='item name.',
+    default=item)
+parser.add_argument(
+    '--title', help='title of variable.',
+    default=title)
+parser.add_argument(
+    '--unit', help='unit of item.',
+    default=unit)
+
+opt = A()
+parser.parse_args(namespace=opt)
+
+if (opt.debug):
+    opt.verbose = True
+
+if (opt.debug):
+    print("dbg:ofile:", opt.ofile)
+    print("dbg:num_times",opt.num_times)
+
+
+
+aitm1, astr1, aend1 = ('GLON64', 1, 64)
+aitm2, astr2, aend2 = ('GGRA32', 1, 32)
+aitm3, astr3, aend3 = ('STDPL17', 1, 17)
+
+d = np.empty(shape=(17,32,64), dtype='>f4')
+
+with GT3File(opt.ofile, mode='wb') as f:
     f.current_header = GT3Header(
         dset='test',
-        item='hoge', unit='-', title='test data',
-        date=date, utim='HOUR',
-        aitm1='GLON64', astr1=1, aend1=64,
-        aitm2='GGLA32', astr2=1, aend2=32,
-        aitm3='SFC1', astr3=1, aend3=1,
+        item=opt.item, unit=opt.unit, title=opt.title,
+        date=date, utim=utim,
+        aitm1=aitm1, astr1=1, aend1=64,
+        aitm2=aitm2, astr2=1, aend2=32,
+        aitm3=aitm3, astr3=1, aend3=17,
     )
 
     f.set_current_data(d)
@@ -27,8 +81,10 @@ with GT3File('testgt3', mode='wb') as f:
     f.write_one_header()
     f.write_one_data()
 
-    for i in range(11):
+    for i in range(opt.num_times-1):
+        d[:,:,:] = d[:,:,:] + 1.0
         time = f.current_header.time + deltaT
+
         f.current_header.set_time_date(time=time)
 
         f.write_one_header()
