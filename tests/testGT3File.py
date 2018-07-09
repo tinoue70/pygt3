292ec7d5b3694a429127007baaa8fc133f3dec91
Takahiro INOUE <tinoue@rist.jp>
2018-07-09 16:29:40 +0900

    Reconstructed tests for GT3File.

--- tests/testGT3File.py
+++ tests/testGT3File.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 import numpy as np
 import unittest
 import tempfile
+from io import StringIO
+from contextlib import redirect_stdout
 
 from pygt3file import GT3File, GT3Header, InvalidArgumentError
 
@@ -18,6 +20,7 @@ class TestGT3File(unittest.TestCase):
         utim = u'hours'
         deltaT = 3
 
+        # single variable multi time file
         with GT3File("test00", 'wb') as f1:
             f1.current_header = GT3Header(
                 dset='test', item='hoge',
@@ -37,6 +40,7 @@ class TestGT3File(unittest.TestCase):
                 f1.write_one_header()
                 f1.write_one_data()
 
+        # multi variable single time file
         with GT3File("test01", 'wb') as f2:
             f2.current_header = GT3Header(
                 dset='test',
@@ -51,10 +55,7 @@ class TestGT3File(unittest.TestCase):
                 f2.write_one_header()
                 f2.write_one_data()
 
-    def test_write_00(self):
-        pass
-
-    def test_read_00(self):
+    def test00_open(self):
         """ Test for opening not exist file. """
         with self.assertRaises(OSError):
             filename = 'prcpr'  # not exist
@@ -62,7 +63,7 @@ class TestGT3File(unittest.TestCase):
             with GT3File(filename, mode) as f:
                 pass
 
-    def test_read_01(self):
+    def test01_open(self):
         """ Test for invalid mode."""
         with self.assertRaises(InvalidArgumentError):
             filename = 'prcpx'
@@ -70,47 +71,113 @@ class TestGT3File(unittest.TestCase):
             with GT3File(filename, mode) as f:
                 pass
 
-    def test_read_02(self):
-        """ Test for writing and reading """
-        expect = (
-            "===== Data table: ==============================================================\n"
-            "    item      time  tdur   utim dfmt                date   aitm1   aitm2 aitm3\n"
-            "0   hoge  17693439     3  hours  UR4 2018-06-16 15:00:00  GLON64  GGLA32  SFC1\n"
-            "1   hoge  17693442     3  hours  UR4 2018-06-16 18:00:00  GLON64  GGLA32  SFC1\n"
-            "2   hoge  17693445     3  hours  UR4 2018-06-16 21:00:00  GLON64  GGLA32  SFC1\n"
-            "3   hoge  17693448     3  hours  UR4 2018-06-17 00:00:00  GLON64  GGLA32  SFC1\n"
-            "4   hoge  17693451     3  hours  UR4 2018-06-17 03:00:00  GLON64  GGLA32  SFC1\n"
-            "5   hoge  17693454     3  hours  UR4 2018-06-17 06:00:00  GLON64  GGLA32  SFC1\n"
-            "6   hoge  17693457     3  hours  UR4 2018-06-17 09:00:00  GLON64  GGLA32  SFC1\n"
-            "7   hoge  17693460     3  hours  UR4 2018-06-17 12:00:00  GLON64  GGLA32  SFC1\n"
-            "8   hoge  17693463     3  hours  UR4 2018-06-17 15:00:00  GLON64  GGLA32  SFC1\n"
-            "9   hoge  17693466     3  hours  UR4 2018-06-17 18:00:00  GLON64  GGLA32  SFC1\n"
-            "10  hoge  17693469     3  hours  UR4 2018-06-17 21:00:00  GLON64  GGLA32  SFC1\n"
-            "11  hoge  17693472     3  hours  UR4 2018-06-18 00:00:00  GLON64  GGLA32  SFC1\n"
-            "================================================================================\n")
-
+    def test10_show_table(self):
+        """ Test show_table() to a stdout. """
+        expect = ("""\
+===== Data table: ==============================================================
+    item      time  tdur   utim dfmt                date   aitm1   aitm2 aitm3
+0   hoge  17693439     3  hours  UR4 2018-06-16 15:00:00  GLON64  GGLA32  SFC1
+1   hoge  17693442     3  hours  UR4 2018-06-16 18:00:00  GLON64  GGLA32  SFC1
+2   hoge  17693445     3  hours  UR4 2018-06-16 21:00:00  GLON64  GGLA32  SFC1
+3   hoge  17693448     3  hours  UR4 2018-06-17 00:00:00  GLON64  GGLA32  SFC1
+4   hoge  17693451     3  hours  UR4 2018-06-17 03:00:00  GLON64  GGLA32  SFC1
+5   hoge  17693454     3  hours  UR4 2018-06-17 06:00:00  GLON64  GGLA32  SFC1
+6   hoge  17693457     3  hours  UR4 2018-06-17 09:00:00  GLON64  GGLA32  SFC1
+7   hoge  17693460     3  hours  UR4 2018-06-17 12:00:00  GLON64  GGLA32  SFC1
+8   hoge  17693463     3  hours  UR4 2018-06-17 15:00:00  GLON64  GGLA32  SFC1
+9   hoge  17693466     3  hours  UR4 2018-06-17 18:00:00  GLON64  GGLA32  SFC1
+10  hoge  17693469     3  hours  UR4 2018-06-17 21:00:00  GLON64  GGLA32  SFC1
+11  hoge  17693472     3  hours  UR4 2018-06-18 00:00:00  GLON64  GGLA32  SFC1
+================================================================================
+""")
         with GT3File('test00') as f:
             f.scan()
-            with tempfile.TemporaryFile('w+') as ff:
-                f.show_table(file=ff)
-                ff.seek(0)
-                result = ff.read()
-
+            with StringIO() as o:
+                with redirect_stdout(o):
+                    f.show_table()
+                    result = o.getvalue()
         self.assertMultiLineEqual(result, expect)
 
-    def test_read_03(self):
-        """ Test for read_current_header """
+    def test20_read_one_header(self):
+        """Test for read_one_header() and skip_one_data()."""
+        expect = u"""\
+2018-06-16T15 17693439 hours
+2018-06-16T18 17693442 hours
+"""
         with GT3File('test00') as f:
             f.scan()
-            f.read_one_header()
-            f.read_one_data()
+            with StringIO() as o:
+                f.read_one_header()
+                f.skip_one_data()
+                print(f.current_header.date,
+                      f.current_header.time,
+                      file=o)
 
-        self.assertTrue(f.current_data.dtype == np.dtype('>f4'))
-        self.assertTupleEqual(f.current_data.shape, (1, 32, 64))
-        self.assertAlmostEqual(f.current_data.mean(), 0.)
+                f.read_one_header()
+                print(f.current_header.date,
+                      f.current_header.time,
+                      file=o)
 
-    def test_extract_t_axis_00(self):
-        """ Test for extract_t_axis() with multi-items file. """
+                result = o.getvalue()
+        self.assertMultiLineEqual(result,expect)
+
+    def test30_read_one_data(self):
+        """Test for read_one_data()."""
+        expect = """
+  C_CONTIGUOUS : True
+  F_CONTIGUOUS : False
+  OWNDATA : False
+  WRITEABLE : True
+  ALIGNED : True
+  WRITEBACKIFCOPY : False
+  UPDATEIFCOPY : False
+>f4
+2048 4
+3 (1, 32, 64)
+"""
+        with GT3File('test00') as f:
+            f.scan()
+            with StringIO() as o:
+                f.read_one_header()
+                f.read_one_data()
+                f.read_one_header()
+                f.read_one_data()
+                with redirect_stdout(o):
+                    print()
+                    print(f.current_data.flags)
+                    print(f.current_data.dtype)
+                    print(f.current_data.size, f.current_data.itemsize)
+                    print(f.current_data.ndim, f.current_data.shape)
+
+                result = o.getvalue()
+        self.assertMultiLineEqual(result,expect)
+
+    def test40_read(self):
+        """ Test for read(). """
+        expect = """\
+2018-06-16T15 0.0
+2018-06-16T18 0.0
+2018-06-16T21 0.0
+2018-06-17T00 0.0
+2018-06-17T03 0.0
+2018-06-17T06 0.0
+2018-06-17T09 0.0
+2018-06-17T12 0.0
+2018-06-17T15 0.0
+2018-06-17T18 0.0
+2018-06-17T21 0.0
+2018-06-18T00 0.0
+"""
+
+        with GT3File('test00') as f:
+            with StringIO() as o:
+                for h, d in f.read():
+                    print(h.date,d[0, 0, 0],file=o)
+                result = o.getvalue()
+        self.assertMultiLineEqual(result, expect)
+
+    def test50_extract_t_axis(self):
+        """Test for extract_t_axis() with multi-items file."""
         ts = np.array([17693439])
         expect = {'values': ts, 'unit': 'hours'}
 
@@ -120,8 +187,8 @@ class TestGT3File(unittest.TestCase):
         self.assertEqual(tuple(result['values']), tuple(expect['values']))
         self.assertEqual(result['unit'], expect['unit'])
 
-    def test_extract_t_axis_01(self):
-        """ Test for extract_t_axis(). """
+    def test51_extract_t_axis(self):
+        """Test for extract_t_axis() with single-item file."""
         ts = np.array(
             [17693439, 17693442, 17693445, 17693448,
              17693451, 17693454, 17693457, 17693460,
@@ -133,48 +200,6 @@ class TestGT3File(unittest.TestCase):
 
         self.assertEqual(tuple(result['values']), tuple(expect['values']))
         self.assertEqual(result['unit'], expect['unit'])
-
-    def test_read_00(self):
-        """ Test for read(). """
-        import io
-        from contextlib import redirect_stdout
-        expect = """\
-2018-06-16T15
-0.0
-2018-06-16T18
-0.0
-2018-06-16T21
-0.0
-2018-06-17T00
-0.0
-2018-06-17T03
-0.0
-2018-06-17T06
-0.0
-2018-06-17T09
-0.0
-2018-06-17T12
-0.0
-2018-06-17T15
-0.0
-2018-06-17T18
-0.0
-2018-06-17T21
-0.0
-2018-06-18T00
-0.0
-"""
-
-        o = io.StringIO()
-        with redirect_stdout(o):
-            with GT3File('test00') as f:
-                for h, d in f.read():
-                    # h.dump()
-                    print(h.date)
-                    print(d[0, 0, 0])
-        result = o.getvalue()
-        o.close()
-        self.assertMultiLineEqual(result, expect)
 
 
 ###############################################################################
