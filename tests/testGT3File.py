3e1067f338e681f856bb640d1ad97253823d4f27
Takahiro INOUE <tinoue@rist.jp>
2018-07-11 16:20:52 +0900

    Now test data has realistic values, not zero.

--- tests/testGT3File.py
+++ tests/testGT3File.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 import numpy as np
 import pandas as pd
 import unittest
-import tempfile
 from io import StringIO
 from contextlib import redirect_stdout
 
@@ -17,9 +16,18 @@ from pygt3file import GT3File, GT3Header, InvalidArgumentError
 class TestGT3File(unittest.TestCase):
     def setUp(self):
         """ write one header and data """
+        self.maxDiff = None
         date = '20180616 150000'
         utim = u'hours'
         deltaT = 3
+        dtype='f4'
+        aitm1, astr1, aend1 = ('GLON64', 1, 64)
+        aitm2, astr2, aend2 = ('GGLA32', 1, 32)
+        aitm3, astr3, aend3 = ('SFC1', 1, 1)
+        x = np.sin(np.linspace(-np.pi*2., np.pi*2., aend1), dtype=dtype)
+        y = np.cos(np.linspace(-np.pi, np.pi, aend2), dtype=dtype)
+        z = np.exp(np.linspace(np.pi, 0., aend3), dtype=dtype)
+        d = np.outer(z,np.outer(y,x)).reshape(aend3, aend2, aend1)
 
         # single variable multi time file
         with GT3File("test00", 'wb') as f1:
@@ -27,17 +35,17 @@ class TestGT3File(unittest.TestCase):
                 dset='test', item='hoge',
                 title='testdata for TestGT3File', unit='-',
                 date=date, utim=utim, tdur=deltaT,
-                aitm1='GLON64', astr1=1, aend1=64,
-                aitm2='GGLA32', astr2=1, aend2=32,
-                aitm3='SFC1', astr3=1, aend3=1)
-            f1.set_current_data(np.zeros(shape=(1, 32, 64), dtype='f4'))
+                aitm1=aitm1, astr1=astr1, aend1=aend1,
+                aitm2=aitm2, astr2=astr1, aend2=aend2,
+                aitm3=aitm3, astr3=astr3, aend3=aend3)
+            f1.set_current_data(d)
             f1.write_one_header()
             f1.write_one_data()
 
             for n in range(11):
                 f1.current_header.set_time_date(
                     time=f1.current_header.time+deltaT)
-                f1.set_current_data(np.zeros(shape=(1, 32, 64), dtype='f4'))
+                f1.set_current_data(d)
                 f1.write_one_header()
                 f1.write_one_data()
 
@@ -73,6 +81,7 @@ class TestGT3File(unittest.TestCase):
                 pass
 
     def test_scan_00(self):
+        """Test scan() with single variable file."""
         expect1 = (12, 12, 1)
         expect2 = pd.Series([
             17693439, 17693442, 17693445, 17693448,
@@ -81,26 +90,25 @@ class TestGT3File(unittest.TestCase):
 
         with GT3File('test00') as f:
             f.scan()
-            result1 = (f.num_of_data,f.num_of_times,f.num_of_items)
+            result1 = (f.num_of_data, f.num_of_times, f.num_of_items)
             result2 = f.table.iloc[:, 1]  # date
 
         self.assertEqual(result1, expect1)
-        self.assertEqual((result2-expect2).mean(),0.0)
+        self.assertEqual((result2-expect2).mean(), 0.0)
 
     def test_scan_01(self):
+        """Test scan() with multi variable file."""
         expect1 = (8, 1, 8)
         expect2 = ["hoge00", "hoge01", "hoge02", "hoge03",
                    "hoge04", "hoge05", "hoge06", "hoge07"]
 
         with GT3File('test01') as f:
             f.scan()
-            result1 = (f.num_of_data,f.num_of_times,f.num_of_items)
+            result1 = (f.num_of_data, f.num_of_times, f.num_of_items)
             result2 = f.table.iloc[:, 0].values.tolist()
 
         self.assertEqual(result1, expect1)
         self.assertEqual(result2, expect2)
-
-
 
     def test_show_table_00(self):
         """ Test show_table() to a stdout. """
@@ -150,7 +158,7 @@ class TestGT3File(unittest.TestCase):
                       file=o)
 
                 result = o.getvalue()
-        self.assertMultiLineEqual(result,expect)
+        self.assertMultiLineEqual(result, expect)
 
     def test_read_one_data_00(self):
         """Test for read_one_data()."""
@@ -181,29 +189,29 @@ class TestGT3File(unittest.TestCase):
                     print(f.current_data.ndim, f.current_data.shape)
 
                 result = o.getvalue()
-        self.assertMultiLineEqual(result,expect)
+        self.assertMultiLineEqual(result, expect)
 
     def test_read_00(self):
         """ Test for read(). """
         expect = """\
-2018-06-16T15 0.0
-2018-06-16T18 0.0
-2018-06-16T21 0.0
-2018-06-17T00 0.0
-2018-06-17T03 0.0
-2018-06-17T06 0.0
-2018-06-17T09 0.0
-2018-06-17T12 0.0
-2018-06-17T15 0.0
-2018-06-17T18 0.0
-2018-06-17T21 0.0
-2018-06-18T00 0.0
+2018-06-16T15 4.0460477e-06
+2018-06-16T18 4.0460477e-06
+2018-06-16T21 4.0460477e-06
+2018-06-17T00 4.0460477e-06
+2018-06-17T03 4.0460477e-06
+2018-06-17T06 4.0460477e-06
+2018-06-17T09 4.0460477e-06
+2018-06-17T12 4.0460477e-06
+2018-06-17T15 4.0460477e-06
+2018-06-17T18 4.0460477e-06
+2018-06-17T21 4.0460477e-06
+2018-06-18T00 4.0460477e-06
 """
 
         with GT3File('test00') as f:
             with StringIO() as o:
                 for h, d in f.read():
-                    print(h.date,d[0, 0, 0],file=o)
+                    print(h.date, d[0, 0, 0], file=o)
                 result = o.getvalue()
         self.assertMultiLineEqual(result, expect)
 
